40608914c777988887d8b1e616acf24f9a7a634f
timelyportfolio <kent.russell@timelyportfolio.com>
2015-08-15 11:29:33 -0500

    improve shiny example and improve css

--- examples/example_shiny.R
+++ examples/example_shiny.R
@@ -7,10 +7,13 @@ calif <- geojson_read(
   system.file("examples/california.geojson",package="geojsonio")
 )
 
-ui <- list(
-  mapshaperOutput("mapshaper1",height = 500, width = 500)
-  ,  tags$div( style = "width:100%; height: 500px;",
-    textOutput("message", container = h3)
+ui <- fluidPage(
+  fluidRow(
+    column( 8, mapshaperOutput("mapshaper1", height = 600) )
+    ,column( 4, leafletOutput( "leafmap", height = 600 ))
+  )
+  ,fluidRow(
+    textOutput("message", container = p)
   )
 )
 
@@ -21,8 +24,19 @@ server <- function(input, output, session) {
     mapshaper( calif )
   })
 
+  output$leafmap <- renderLeaflet({
+    leaflet( ) %>%
+      addGeoJSON( calif ) %>%
+      fitBounds( -124, 32, -113, 43 )
+  })
+
   observeEvent(input$mapshaper1_export, {
     v$msg <- paste("Exported", input$mapshaper1_export$content)
+    output$leafmap <- renderLeaflet(
+      leaflet( ) %>%
+        addGeoJSON( input$mapshaper1_export$content ) %>%
+        fitBounds( -124, 32, -113, 43 )
+    )
   })
 
   output$message <- renderText(v$msg)
